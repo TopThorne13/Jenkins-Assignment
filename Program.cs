ff6665e304646709a16821ac387f7fa71e7a5e2b
Balpreet Singh <balpreet.singh@nagarro.com>
2024-04-03 11:36:40 +0530

    Latest: Initial Push for Jenkins

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld_JenkinsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
